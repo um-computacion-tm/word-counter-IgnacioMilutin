9722dd049cc8c895f64f540d8dadd69917ed7be0
IgnacioMilutin <i.milutin@alumno.um.edu.ar>
2023-04-06 21:17:11 -0300

    mas de una palabra

--- count_words.py
+++ count_words.py
@@ -1,14 +1,33 @@
 import unittest
 
 def count_words(word):
-    return {word: 1}
+    wordlist = word.split()
+    wordq={}
+    for i in wordlist:
+        if i in wordq:
+            wordq[i]+=1
+        else: 
+            wordq[i]=1
+    return wordq
+
+
 
 class TestCountLetters(unittest.TestCase):
 
     def test_simple(self):
         result = count_words('hola')
         self.assertEqual(result, {'hola': 1})
-
+    def test2(self):
+        result = count_words('hola como hola estas')
+        self.assertEqual(
+            result,
+            {
+                'hola':2,
+                'como':1,
+                'hola':2,
+                'estas':1
+            }
+        )
 
 if __name__ == '__main__':
     unittest.main()
