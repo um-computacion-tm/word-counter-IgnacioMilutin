4f2bfada95b43d57d706d7b8a4ce0c78c6929351
IgnacioMilutin <i.milutin@alumno.um.edu.ar>
2023-04-09 21:27:55 -0300

    sin test

--- count_words.py
+++ count_words.py
@@ -1,5 +1,3 @@
-import unittest
-
 def count_words(word):
     wordlist = word.split()
     wordq={}
@@ -9,25 +7,3 @@ def count_words(word):
         else: 
             wordq[i]=1
     return wordq
-
-
-
-class TestCountLetters(unittest.TestCase):
-
-    def test_simple(self):
-        result = count_words('hola')
-        self.assertEqual(result, {'hola': 1})
-    def test2(self):
-        result = count_words('hola como hola estas')
-        self.assertEqual(
-            result,
-            {
-                'hola':2,
-                'como':1,
-                'hola':2,
-                'estas':1
-            }
-        )
-
-if __name__ == '__main__':
-    unittest.main()
